297f3ef64e9b62bf7788aa776be76704e375c54c
dienigma <joshi.chinmay09@gmail.com>
2019-04-08 19:05:31 +0530

    Made a homepage at '/home'

--- demo/server.js
+++ demo/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-    res.send("Hello Express")
+    res.send("Hello Chinmay")
 })
+
+app.get('/home',(req,res)=> res.send("This is my home page"))
 
 app.listen(3000,()=> console.log(`Server Running on port 3000...`))
