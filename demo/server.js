4558c7f7552534e1bea0a86e6743bceccc4faa5b
dienigma <joshi.chinmay09@gmail.com>
2019-04-08 19:30:30 +0530

    Build login and logout routes

--- demo/server.js
+++ demo/server.js
@@ -7,9 +7,13 @@ app.get('/',(req,res) => {
 
 app.get('/home',(req,res)=> res.send("This is my home page"))
 
-app.get('/home/:id', (req,res) => {
+app.get('/home/:name', (req,res) => {
     console.log(req.params)
-    res.send(`Hey welcome ${req.params.id}`)
+    res.send(`Hey welcome ${req.params.name}`)
 })
+
+app.get('/login',(req,res) => res.send('Successfully Logged in.'))
+
+app.get('/logout',(req,res) => res.send("Logged out"))
 
 app.listen(3000,()=> console.log(`Server Running on port 3000...`))
