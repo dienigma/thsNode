63d1fa7560ab7e37d73c770651f19a13abda766c
dienigma <joshi.chinmay09@gmail.com>
2019-04-10 15:11:17 +0530

    Almost done with the student route.

--- demo/routes/student.js
+++ demo/routes/student.js
@@ -7,7 +7,8 @@ const db = require('../models')
 router.get('/',(req,res) => {
     db.Student.find()
     .then((studentData) =>{
-        
+        // studentData.forEach(element => res.json(element))
+        res.json(studentData)
     })
     .catch(err => res.send(err))
 })
@@ -17,6 +18,27 @@ router.post('/',(req,res)=> {
     console.log(req.body)
     db.Student.create(req.body)
     .then(studentData => res.json({name: studentData.name, number: studentData.phone}))
+    .catch(err => res.send(err))
+})
+
+// find by id routes
+
+router.get('/:id',(req,res)=> {
+    db.Student.findById(req.params.id)
+    .then(studentData => res.render('student',{studentData: studentData}))
+    .catch(err => res.json(err))
+})
+
+// Find by ID and update
+router.put('/:id',(req,res) => {
+    db.Student.findByIdAndUpdate(req.params.id,req.body)
+    .then(studentData => res.json(studentData))
+})
+
+// Delete the object
+router.delete('/:id',(req,res) => {
+    db.Student.findOneAndRemove({"_id":req.params.id})
+    .then(studentData => res.send('Deleted'))
     .catch(err => res.send(err))
 })
 
